97df86c24fc16b285780a01c1525d517c66277d8
kazezinhu <45982803+Kazezinhu@users.noreply.github.com>
2024-07-16 11:47:55 +0100

    Music id check and duration retrieval

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import json
+import os.path
+
 import requests
 import vlc
 import time
 
 # u, t, s, f, v, c
 stream_url_parameter = ['Guest', '2dc98a10999257c14a0920f57d129b02', '318fe6', 'json', '1.8.0', 'NavidromeUI']
-test_id = '14b5f2fd652855752e115730d88488c2'
+test_id = '14b5f2fd652855752e115730d88488c2'  # city ruins
+# ancients = caa0ddc79bccc7f904b0f9192c9949a0
+# hills = 3aab83f9ed78bbdacebdf86b74ca8db7
 
-url = "https://www.squidify.org/api/song/"
-response = requests.get(url)
-data = response.json()
+
+def update_data():
+    url = "https://www.squidify.org/api/song/"
+    response = requests.get(url)
+    data = json.loads(response.content)
+    f = open("data.json", "w")
+    f.write(response.content.decode("utf-8"))
+    f.close()
+    return data
+
+
+def retrieve_data():
+    f = open("data.json", "r")
+    data = json.loads(f.read())
+    f.close()
+    return data
+
+
+data = 0
+if os.path.isfile("data.json"):
+    data = retrieve_data()
+else:
+    data = update_data()
+
+
+def reload_data():
+    global data
+    data = update_data()
+
+
+def get_line(id: str):
+    for item in data:
+        if item["id"] == id:
+            return item
 
 
 def get_url(id: str):
@@ -20,17 +55,34 @@ def get_url(id: str):
 # f.write(response.content.decode("utf-8"))
 # f.close()
 
-audio_stream = requests.get(get_url(test_id), stream=True)
 instance = vlc.Instance()
 player = instance.media_player_new()
-media = instance.media_new(get_url(test_id))
-
-player.set_media(media)
 player.audio_set_track(1)
-player.audio_set_volume(70)
-player.play()
-print("VLC State: ", player.get_state())
-time.sleep(332.24)
+player.audio_set_volume(50)
+
+song_id = test_id
+
+close = False
+while close is False:
+
+    requested_song = input("Enter the song id: ")
+
+    results = get_line(requested_song)
+
+    if results is not None:
+        song_id = requested_song
+    else:
+        print('Music not found.')
+        continue
+
+    length = float(results.get('duration'))
+
+    media = instance.media_new(get_url(song_id))
+    player.set_media(media)
+    time.sleep(1)
+    player.play()
+    print("VLC State: ", player.get_state())
+    time.sleep(length)
 
 
 # media.parse_with_options(1, 0)
@@ -43,7 +95,7 @@ time.sleep(332.24)
         # prev = m
 
 # pya = pyaudio.PyAudio()
-
+# audio_stream = requests.get(get_url(id), stream=True)
 # stream = pya.open(format=pya.get_format_from_width(2), channels=2, rate=44100, output=True, output_device_index=6)
 # stream.write(audio_stream.content[0].to_bytes())
 # stream.stop_stream()
