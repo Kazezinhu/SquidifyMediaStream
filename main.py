f5d1ac80a686925cd3272bf7432c41124e0f8da4
kazezinhu <45982803+Kazezinhu@users.noreply.github.com>
2024-08-05 20:55:02 +0100

    Added search function
    Added recurring prompt when next item plays;
    Reorganized functions for recurrent use;
    Fixed errors verifying if an album or song exists.

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ global t_duration, t_id, t_album, t_title
 mediaplaylist = []
 current = -1
 
+current_prompt = ""
+
 
 def player_free():
     state = player.get_state()
@@ -60,9 +62,20 @@ def next_item_set(self, event):
     current += 1
     set_play()
     print(f"Now playing: \n Album: {t_album}\n Title: {t_title}\n Duration: {t_duration}")
+    print(current_prompt)
+
+
+# checks if the player is playing and then clears the playlist
+def check_player():
+    global medialist, current
+    if player.get_state() == vlc.State.Ended or player.get_state() == vlc.State.Stopped:
+        mediaplaylist.clear()
+        medialist = instance.media_list_new()
+        current = -1
 
 
 async def play_album(album):
+    check_player()
     for track in album:
         medialist.add_media(rq.get_url(track.get('id')))
         mediaplaylist.append(track)
@@ -71,6 +84,24 @@ async def play_album(album):
         await play()
     else:
         print("Queued: " + album[0].get('album'))
+
+
+async def play_track(track):
+    check_player()
+
+    medialist.add_media(rq.get_url(track.get('id')))
+    mediaplaylist.append(track)
+    if player.is_playing():
+        print("Queued: " + track.get('title'))
+    else:
+        player.next()
+        await play()
+
+
+async def show_album_tracks(album):
+    for i, song in enumerate(album, start=1):
+        duration = str(datetime.timedelta(seconds=song.get('duration')))[:-7]
+        print(f"{i} - {song.get('title')} | {duration}")
 
 
 def stop():
@@ -82,6 +113,7 @@ def stop():
 
 
 def show_list():
+    print("Current playlist:")
     for i in range(0, len(mediaplaylist)):
         track = mediaplaylist[i]
         msg = f"{i} : {track.get('title')}"
@@ -90,21 +122,123 @@ def show_list():
         print(msg)
 
 
+async def search():
+    global current_prompt
+
+    current_prompt = "What do you want to search?\n1: Album\n2: Song\n0: Exit\nSelection: "
+    is_album = False
+
+    while True:
+        search_type = input("What do you want to search?\n1: Album\n2: Song\n0: Exit\nSelection: ")
+
+        match search_type:
+            case '0':
+                return
+            case '1':
+                is_album = True
+                break
+            case '2':
+                is_album = False
+                break
+            case _:
+                continue
+
+    current_prompt = "Search: "
+    print("Type 'exit' to exit.")
+    value = input("Search: ")
+
+    if value == "exit":
+        return
+
+    result = rq.search_result(value, is_album)
+
+    if result is None:
+        print("No results found.")
+        return
+
+    for index, item in enumerate(result, start=1):
+        if is_album:
+            content = item.get("name") + ", by " + item.get("albumArtist") + ", " + str(item.get("songCount")) + " tracks, released in " + item.get("date")
+        else:
+            content = item.get("title") + ", from " + item.get("album") + ", by " + item.get("artist") + ", released in " + item.get("date")
+        print(f"{index} - {content}")
+
+    current_prompt = "Select result: "
+    while True:
+        print("Type '0' to return to search, 'q' to return to main program.")
+        value = input("Select result: ")
+
+        if value == "0":
+            await search()
+            break
+        if value == "q":
+            break
+
+        try:
+            selected = int(value) - 1
+        except ValueError:
+            continue
+
+        if selected <= 0 or selected +1 >= len(result):
+            continue
+
+        current_prompt = "Select option: "
+        if not is_album:
+            print("Options:\n1: Play\n2: Download\n0: Exit")
+            while True:
+                value = input("Select option: ")
+
+                match value:
+                    case "0":
+                        return
+                    case "1":
+                        await play_track(rq.request_song_data(result[selected].get("id")))
+                        break
+                    case "2":
+                        await rq.song_dl(result[selected].get("id"))
+                        break
+                    case _:
+                        continue
+        else:
+            while True:
+                print("Options:\n1: Play\n2: Show Tracks\n0: Exit")
+                value = input("Select option: ")
+                match value:
+                    case "0":
+                        return
+                    case "1":
+                        album = rq.request_album_data(result[selected].get("id"))
+                        await play_album(album)
+                        break
+                    case "2":
+                        album = rq.request_album_data(result[selected].get("id"))
+                        await show_album_tracks(album)
+                    case _:
+                        continue
+
+        break
+
+    return
+
+
 async def main():
-    global medialist, current
+    global medialist, current, current_prompt
     event_manager = player.event_manager()
     event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, song_ended, 1)
     event_manager.event_attach(vlc.EventType.MediaListEndReached, media_list_ended, 1)
     event_manager.event_attach(vlc.EventType.MediaListPlayerNextItemSet, next_item_set, 1)
     close = False
     print("Enter 'q' to quit.")
-    print("Enter 'p' to pause/resume, 'stop' to stop and 'volume' to change volume.")
+    print("Enter 'p' to pause/resume and 'volume' to change volume.")
+    print("Enter 'stop' to stop and clear playlist.")
     print("Enter 'next' and 'prev' to control player.")
     print("Enter 'list' to view the current playlist.")
+    print("Enter 'search' to search.")
     print("Types: a - album, t - track, get - get album, dl - download track")
     while close is False:
 
-        request_type = input("Enter action type:\n")
+        current_prompt = "Enter action type: "
+        request_type = input("Enter action type: ")
 
         match request_type:
 
@@ -148,6 +282,9 @@ async def main():
                 vol = int(input('Enter volume: '))
                 player.get_media_player().audio_set_volume(vol)
                 continue
+            case 'search':
+                await search()
+                continue
             case 'q':
                 break
             case _:
@@ -161,17 +298,16 @@ async def main():
                 print('Album not found.')
                 continue
 
-            if album[0].get('error') is not None:
-                print('Album not found.')
-                continue
-
-            for song in album:
-                print(f"{song.get('title')} - {song.get('id')}")
+            await show_album_tracks(album)
 
         if request_type == 'a':
             album = rq.request_album_data(request_id)
 
             if album is None:
+                print('Album not found.')
+                continue
+
+            if album[0] is None:
                 print('Album not found.')
                 continue
 
@@ -192,18 +328,7 @@ async def main():
                 print('Track not found.')
                 continue
 
-            if player.get_state() == vlc.State.Ended or player.get_state() == vlc.State.Stopped:
-                mediaplaylist.clear()
-                medialist = instance.media_list_new()
-                current = 0
-
-            medialist.add_media(rq.get_url(track.get('id')))
-            mediaplaylist.append(track)
-            if player.is_playing():
-                print("Queued: " + track.get('title'))
-            else:
-                player.next()
-                await play()
+            await play_track(track)
 
     player.stop()
 
--- request.py
+++ request.py
@@ -21,16 +21,24 @@ def request_song_data(id: str):
     return json.loads(response.content)
 
 
-def get_url(id: str):
-    return f"https://www.squidify.org/rest/stream?u={stream_url_parameter[0]}&t={stream_url_parameter[1]}&s={stream_url_parameter[2]}&f={stream_url_parameter[3]}&v={stream_url_parameter[4]}&c={stream_url_parameter[5]}&id={id}"
+def get_url(song_id: str):
+    return f"https://www.squidify.org/rest/stream?u={stream_url_parameter[0]}&t={stream_url_parameter[1]}&s={stream_url_parameter[2]}&f={stream_url_parameter[3]}&v={stream_url_parameter[4]}&c={stream_url_parameter[5]}&id={song_id}"
 
 
-async def song_dl(id: str):
-    info = request_song_data(id)
+def search_result(arg: str, is_album: bool):
+    if is_album:
+        response = requests.get(f"https://www.squidify.org/api/album?_end=36&_order=ASC&_sort=name&_start=0&name={arg}")
+    else:
+        response = requests.get(f"https://www.squidify.org/api/song?_end=15&_order=ASC&_sort=title&_start=0&title={arg}")
+    return json.loads(response.content)
+
+
+async def song_dl(song_id: str):
+    info = request_song_data(song_id)
     if info is None or info.get('error') is not None:
         print("Error downloading track")
         return
-    url = get_url(id)
+    url = get_url(song_id)
     response = requests.get(url)
     f = open(info.get('title') + ".flac", "wb")
     f.write(response.content)
